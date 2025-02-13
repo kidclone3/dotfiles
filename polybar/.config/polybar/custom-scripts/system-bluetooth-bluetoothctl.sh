d122fa7bc0ac0bfbc34cce5633ce0aa13f077928
delus <kid.clone.3@gmail.com>
2025-02-13 11:58:54 +0700

    fix missing battery remain of bluetooth

--- polybar/.config/polybar/custom-scripts/system-bluetooth-bluetoothctl.sh
+++ polybar/.config/polybar/custom-scripts/system-bluetooth-bluetoothctl.sh
@@ -1,20 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 bluetooth_print() {
     bluetoothctl | grep --line-buffered 'Device\|#' | while read -r REPLY; do
         if [ "$(systemctl is-active "bluetooth.service")" = "active" ]; then
             printf '#1'
 
-            devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
+            # Get the list of Bluetooth cards
+            devices_paired=$(pactl list short cards | grep bluez_card | awk '{print $2}' | tac)
             counter=0
 
             for device in $devices_paired; do
-                device_info=$(bluetoothctl info "$device")
+                device_info=$(pactl list cards | grep -A 20 "$device")
 
-                if echo "$device_info" | grep -q "Connected: yes"; then
-                    device_output=$(echo "$device_info" | grep "Alias" | cut -d ' ' -f 2-)
-                    device_battery_percent=$(echo "$device_info" | grep "Battery Percentage" | awk -F'[()]' '{print $2}')
-
+                if echo "$device_info" | grep -q "a2dp_sink"; then
+                    device_output=$(echo "$device_info" | grep "device.description = " | cut -d ' ' -f 3- | tr -d '"')
+                    device_battery_percent=$(echo "$device_info" | grep "bluetooth.battery" | awk -F'[=]' '{print $2}' | tr -d ' %')
+                    device_battery_percent=$(printf "%d" "$device_battery_percent")
                     if [ -n "$device_battery_percent" ]; then
                         if [ "$device_battery_percent" -gt 90 ]; then
                             device_battery_icon=""
@@ -28,13 +29,13 @@ bluetooth_print() {
                             device_battery_icon=""
                         fi
 
-                        device_output="$device_output $device_battery_icon $device_battery_percent%"
+                        display_output="$device_output $device_battery_icon $device_battery_percent%"
                     fi
 
                     if [ $counter -gt 0 ]; then
-                        printf ", %s" "$device_output"
+                        printf ", %s" "$display_output"
                     else
-                        printf " %s" "$device_output"
+                        printf " %s" "$display_output"
                     fi
 
                     counter=$((counter + 1))
@@ -52,18 +53,20 @@ bluetooth_toggle() {
     if bluetoothctl show | grep -q "Powered: no"; then
         bluetoothctl power on >> /dev/null
         sleep 1
-
-        devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
-        echo "$devices_paired" | while read -r line; do
+        notify-send "Bluetooth" "Bluetooth turned on, pairing latest device"
+        while IFS= read -r line; do
             bluetoothctl connect "$line" >> /dev/null
-        done
+        done < <(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
+        notify-send "Bluetooth" "Tried to connect to all paired devices"
     else
-        devices_paired=$(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2)
-        echo "$devices_paired" | while read -r line; do
-            bluetoothctl disconnect "$line" >> /dev/null
-        done
-
-        bluetoothctl power off >> /dev/null
+        # Show confirmation dialog using rofi
+        ans=$(echo -e "No\nYes" | rofi -dmenu -i -p "Do you want to disconnect all paired Bluetooth devices?")
+        if [[ $ans == "Yes" ]]; then
+            while IFS= read -r line; do
+                bluetoothctl disconnect "$line" >> /dev/null
+            done < <(bluetoothctl devices Paired | grep Device | cut -d ' ' -f 2 | tac)
+            bluetoothctl power off >> /dev/null
+        fi
     fi
 }
 
